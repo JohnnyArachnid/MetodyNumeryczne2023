adcb7986f83663c137d841112a92c87b48c20e5c
JohnnyArachnid <daniel.szarek@student.uj.edu.pl>
2023-12-14 22:30:56 +0100

    Zmiany

--- Zadanie1/main.cpp
+++ Zadanie1/main.cpp
@@ -8,6 +8,10 @@ using namespace std;
 int main(){
     double wartosc_funkcji = 0;
     double x = 1;
+    double x2 = x*x;
+    double x4 = x2*x2;
+    double e0 = exp(-1);
+    double e3 = e0*e0*e0;
     for(int i = 0; i <= 22; i++) {
         double wartosc_cos = cos(i*x*x*x*x);
         double wartosc_e = exp(-i);
